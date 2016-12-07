821e7baa9a8582c5bd8abfa27ed31f4858a79e5b
Che B <cnakamura336@gmail.com>
2016-12-07 18:20:18 -0500

    update ex3.js

--- ex3-fp.js
+++ ex3-fp.js
@@ -1,26 +1,55 @@
-var fs = require('fs');
+var fs = require('fs-promise');
 var _ = require('lodash');
 var file1 = process.argv[2];
 var file2 = process.argv[3];
 var outputFile = process.argv[4];
 
-fs.readFile(file1, function(err, buffer) {
-  if (err) {
-    console.log('Something went wrong because', err.message);
-    return;
-  }
-  var file1Lines = buffer.toString().split('\n');
-  fs.readFile(file2, function(err, buffer) {
-    if (err) {
-      console.log('Something went wrong because', err.message);
-      return;
-    }
-    var file2Lines = buffer.toString().split('\n');
-    fs.writeFile(outputFile, _.flatten(_.zip(file1Lines, file2Lines)).join('\n'), function(err) {
-      if (err) {
-        console.log('Something went wrong because', err.message);
-        return;
-      }
-    });
+// fs.readFile(file1, function(err, buffer) {
+//   if (err) {
+//     console.log('Something went wrong because', err.message);
+//     return;
+//   }
+//   var file1Lines = buffer.toString().split('\n');
+//   fs.readFile(file2, function(err, buffer) {
+//     if (err) {
+//       console.log('Something went wrong because', err.message);
+//       return;
+//     }
+//     var file2Lines = buffer.toString().split('\n');
+//     fs.writeFile(outputFile, _.flatten(_.zip(file1Lines, file2Lines)).join('\n'), function(err) {
+//       if (err) {
+//         console.log('Something went wrong because', err.message);
+//         return;
+//       }
+//     });
+//   });
+// });
+
+fs.readFile(file1)
+  .then(function(buffer) {
+    var file1_content = buffer.toString().split('\n');
+    console.log("File1 content: ", file1_content);
+    return fs.readFile(file2);
+  })
+  .then(function(buffer) {
+    var file2_content = buffer.toString().split('\n');
+    console.log("File2 content: ", file2_content);
+    return fs.writeFile(outputFile, _.flatten(_.zip(file1_content, file2_content)).join('\n'));
+  })
+  .then(function() {
+    console.log('Worked out!');
+  })
+  .catch(function(err) {
+    console.log("Something went wrong because ", err.massage);
+    return ;
   });
-});
+
+
+
+
+
+
+
+
+
+  //
---
+++ readit2.md
@@ -0,0 +1,1 @@
+aoi\nsdvfio\ndododo\n
