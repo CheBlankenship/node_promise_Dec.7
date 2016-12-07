7e39b8fb2a3df07e2373994b9838edd07badae7d
Che B <cnakamura336@gmail.com>
2016-12-07 16:55:33 -0500

    finish ex1.js

--- ex1.js
+++ ex1.js
@@ -1,12 +1,22 @@
-var fs = require('fs');
+var fs = require('fs-promise');
 
-var filename = process.argv[2];
-fs.readFile(filename, function(err, buffer) {
-  if (err) {
-    console.log('Something went wrong.');
-    console.log('Because: ', err.message);
+var file1 = process.argv[2];
+// fs.readFile(filename, function(err, buffer) {
+//   if (err) {
+//     console.log('Something went wrong.');
+//     console.log('Because: ', err.message);
+//     return;
+//   }
+//   var content = buffer.toString();
+//   console.log(content.toUpperCase());
+// });
+
+fs.readFile(file1)
+  .catch(function(err) {
+    console.log("SOME THING IS WRONG");
+    console.log("Becouse: ", err.message);
     return;
-  }
-  var content = buffer.toString();
-  console.log(content.toUpperCase());
-});
+  })
+  .then(function(buffer) {
+    console.log("contents of file1: ", buffer.toString().toUpperCase());
+  });
