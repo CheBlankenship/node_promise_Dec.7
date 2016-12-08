4104312fb25ce5e0ddbf97f179e9811423b1325e
Che B <cnakamura336@gmail.com>
2016-12-08 10:21:53 -0500

    finished ex3.js

--- ex3-fp.js
+++ ex3-fp.js
@@ -5,26 +5,26 @@ var file1 = process.argv[2];
 var file2 = process.argv[3];
 var outputFile = process.argv[4];
 
-// fs.readFile(file1, function(err, buffer) {
-//   if (err) {
-//     console.log('Something went wrong because', err.message);
-//     return;
-//   }
-//   var file1Lines = buffer.toString().split('\n');
-//   fs.readFile(file2, function(err, buffer) {
-//     if (err) {
-//       console.log('Something went wrong because', err.message);
-//       return;
-//     }
-//     var file2Lines = buffer.toString().split('\n');
-//     fs.writeFile(outputFile, _.flatten(_.zip(file1Lines, file2Lines)).join('\n'), function(err) {
-//       if (err) {
-//         console.log('Something went wrong because', err.message);
-//         return;
-//       }
-//     });
-//   });
-// });
+fs.readFile(file1, function(err, buffer) {
+  if (err) {
+    console.log('Something went wrong because', err.message);
+    return;
+  }
+  var file1Lines = buffer.toString().split('\n');
+  fs.readFile(file2, function(err, buffer) {
+    if (err) {
+      console.log('Something went wrong because', err.message);
+      return;
+    }
+    var file2Lines = buffer.toString().split('\n');
+    fs.writeFile(outputFile, _.flatten(_.zip(file1Lines, file2Lines)).join('\n'), function(err) {
+      if (err) {
+        console.log('Something went wrong because', err.message);
+        return;
+      }
+    });
+  });
+});
 
 // Promise.all([fs.readFile(file1),fs.readFile(file2)])
 //   .spread(function(buffer1, buffer2) {
--- ex3.js
+++ ex3.js
@@ -1,31 +1,46 @@
-var fs = require('fs');
+var fs = require('fs-promise');
 
 var file1 = process.argv[2];
 var file2 = process.argv[3];
 var outputFile = process.argv[4];
 
-fs.readFile(file1, function(err, buffer) {
-  if (err) {
-    console.log('Something went wrong because', err.message);
-    return;
-  }
-  var file1Lines = buffer.toString().split('\n');
-  fs.readFile(file2, function(err, buffer) {
-    if (err) {
-      console.log('Something went wrong because', err.message);
-      return;
-    }
-    var file2Lines = buffer.toString().split('\n');
+// fs.readFile(file1, function(err, buffer) {
+//   if (err) {
+//     console.log('Something went wrong because', err.message);
+//     return;
+//   }
+//   var file1Lines = buffer.toString().split('\n');
+//   fs.readFile(file2, function(err, buffer) {
+//     if (err) {
+//       console.log('Something went wrong because', err.message);
+//       return;
+//     }
+//     var file2Lines = buffer.toString().split('\n');
+//     var allLines = [];
+//     file1Lines.forEach(function(line, idx) {
+//       allLines.push(line);
+//       allLines.push(file2Lines[idx]);
+//     });
+//     fs.writeFile(outputFile, allLines.join('\n'), function(err) {
+//       if (err) {
+//         console.log('Something went wrong because', err.message);
+//         return;
+//       }
+//     });
+//   });
+// });
+
+Promise.all([fs.readFile(file1), fs.readFile(file2)])
+  .then(function(buffers) {
+    var file1Lines = buffers[0].toString().split('\n');
+    var file2Lines = buffers[1].toString().split('\n');
     var allLines = [];
     file1Lines.forEach(function(line, idx) {
       allLines.push(line);
       allLines.push(file2Lines[idx]);
     });
-    fs.writeFile(outputFile, allLines.join('\n'), function(err) {
-      if (err) {
-        console.log('Something went wrong because', err.message);
-        return;
-      }
-    });
+    fs.writeFile(outputFile, allLines.join('\n'));
+  })
+  .catch(function(err) {
+    console.log("Some thing is wrong", err.message);
   });
-});
